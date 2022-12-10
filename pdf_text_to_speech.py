d8fc1323e06bc8249c90951ec2be4d194ea01843
P A R Z I V A L <65802000+parzivalsupreme@users.noreply.github.com>
2022-12-10 21:45:08 +0800

    Update pdf_text_to_speech.py

--- pdf_text_to_speech.py
+++ pdf_text_to_speech.py
@@ -1,32 +1,33 @@
+#Modules
 from PyPDF2 import PdfReader
 import pyttsx3
 
-
+#Text to Speech
 engine = pyttsx3.init ('sapi5')
 voices = engine.getProperty('voices')
 VoiceGender = engine.setProperty('voice', voices[0].id)
 engine.setProperty('rate', 175)
-
-
 def speak(texttospeech):
     engine.say(texttospeech)
     engine.runAndWait()
 
 
-reader =PdfReader(r"C:\Users\Elijah\Downloads\The Handmaids Tale [full text].pdf")
+reader =PdfReader(r"C:\Users\Elijah\Downloads\The Handmaids Tale [full text].pdf") #PDF file path
+#PDF page input up to 5 pages
 page1 = reader.pages[int(input("Page: "))]
 page2 = reader.pages[int(input("Page: "))]
 page3 = reader.pages[int(input("Page: "))]
 page4 = reader.pages[int(input("Page: "))]
 page5 = reader.pages[int(input("Page: "))]
 
+#Text extraction
 text1 = page1.extract_text()
 text2 = page2.extract_text()
 text3 = page3.extract_text()
 text4 = page4.extract_text()
 text5 = page5.extract_text()
 
-
+#Execute
 while True:
  print(text1)
  speak (text1)
