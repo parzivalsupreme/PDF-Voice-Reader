55b24b0a69bd16e99cb308cbf6dc25567b3d5cba
P A R Z I V A L <65802000+parzivalsupreme@users.noreply.github.com>
2022-12-17 20:40:44 +0800

    Add files via upload

--- pdf_text_to_speech.py
+++ pdf_text_to_speech.py
@@ -1,25 +1,33 @@
+#Imports
 from PyPDF2 import PdfReader
 import pyttsx3
 
 
+#Initialize the text to speech engine and set the voice to the first voice in the list of voices.
 engine = pyttsx3.init ('sapi5')
 voices = engine.getProperty('voices')
 VoiceGender = engine.setProperty('voice', voices[0].id)
+#Set the rate of the voice to 175.
 engine.setProperty('rate', 175)
 
 
+#Define a function that takes in a string of text and converts it to speech using the engine.
 def speak(texttospeech):
     engine.say(texttospeech)
     engine.runAndWait()
 
 
+#PDF file path
 reader =PdfReader(r"C:\Users\Elijah\Downloads\The Handmaids Tale [full text].pdf")
+#Read in the PDF file and store the pages in variables.
 page1 = reader.pages[int(input("Page: "))]
 page2 = reader.pages[int(input("Page: "))]
 page3 = reader.pages[int(input("Page: "))]
 page4 = reader.pages[int(input("Page: "))]
 page5 = reader.pages[int(input("Page: "))]
 
+
+#Extract the text from each page and store it in a separate variable.
 text1 = page1.extract_text()
 text2 = page2.extract_text()
 text3 = page3.extract_text()
@@ -27,6 +35,7 @@ text4 = page4.extract_text()
 text5 = page5.extract_text()
 
 
+#Infinite loop that prints the text from each page and converts it to speech.
 while True:
  print(text1)
  speak (text1)
